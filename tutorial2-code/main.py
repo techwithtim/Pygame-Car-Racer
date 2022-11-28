0253562c75efc349b08136e4b568130edee177eb
Alon Malka <alon1malka@gmail.com>
2022-11-28 12:48:25 +0200

    new trace
    the trace looks diffrent, built from lines rn

--- tutorial2-code/main.py
+++ tutorial2-code/main.py
@@ -1,10 +1,38 @@
 import pygame
 import time
 import math
-from utils import scale_image, blit_rotate_center
+from utils import scale_image, blit_rotate_center, draw_sensors
 
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
+
+LINES = [((35, 130), (35, 650)),
+    ((35, 650), (160, 770)),
+    ((160, 770), (670, 770)),
+    ((670, 770), (760, 640)),
+    ((760, 640), (760, 110)),
+    ((760, 110), (670, 20)),
+    ((670, 20), (430, 20)),
+    ((170, 200), (170, 600)),
+    ((170, 600), (240, 660)),
+    ((240, 660), (580, 670)),
+    ((580, 670), (660, 590)),
+    ((660, 590), (660, 190)),
+    ((35, 130), (130, 20)),
+    ((130, 20), (220, 20)),
+    ((220, 20), (300, 110)),
+    ((300, 110), (300, 510)),
+    ((430, 20), (370, 90)),
+    ((370, 90), (370, 180)),
+    ((370, 180), (420, 240)),
+    ((420, 240), (500, 240)),
+    ((660, 190), (620, 130)),
+    ((620, 130), (500, 130)),
+    ((500, 240), (560, 300)),
+    ((560, 300), (560, 510)),
+    ((560, 510), (500, 560)),
+    ((500, 560), (330, 560)),
+    ((330, 560), (300, 510))]
 
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
 TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
@@ -43,12 +71,7 @@ class AbstractCar:
 
     def draw(self, win):
         blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
-
-    # def draw_sensor(self, win):
-    #     for i in range(8):
-    #         rect = self.img.get_rect
-    #         print(rect)
-    #         pygame.draw.line(win, "black", center, (0,0))
+        self.sensors = draw_sensors(win, self.img, (self.x, self.y), self.angle)
 
     def move_forward(self):
         self.vel = min(self.vel + self.acceleration, self.max_vel)
@@ -66,11 +89,29 @@ class AbstractCar:
         self.y -= vertical
         self.x -= horizontal
 
-    def collide(self, mask, x=0, y=0):
-        car_mask = pygame.mask.from_surface(self.img)
-        offset = (int(self.x - x), int(self.y - y))
-        poi = mask.overlap(car_mask, offset)
-        return poi
+    def collide(self):
+        car_rect = self.img.get_rect(x=self.x, y=self.y)
+        for line in LINES:
+            if car_rect.clipline(line):
+                return True
+        return False
+
+        # if car_rect.clipline((1,1),(800,200)):
+        #     print("BOOM")
+        # car_mask = pygame.mask.from_surface(self.img)
+        # offset = (int(self.x - x), int(self.y - y))
+        # check collision with lines
+        # poi = mask.overlap(car_mask, offset)
+
+    def sense(self, win):
+        # print(TRACK_BORDER_MASK.outline())
+            # pygame.draw.circle(win,"black", point, 15)
+        # for point in *TRACK_BORDER_MASK.outline():
+        # pygame.draw.lines(win, "Pink", *TRACK_BORDER_MASK.outline())
+        for i in range(0, len(self.sensors), 2):
+            # print(self.sensors[i])
+            # print(self.sensors[i+1])
+            pass
 
     def reset(self):
         self.x, self.y = self.START_POS
@@ -80,9 +121,10 @@ class AbstractCar:
     def update_score(self, score):
         self.score += score
 
+
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
-    START_POS = (180, 200)
+    START_POS = (240, 310)
 
     def reduce_speed(self):
         self.vel = max(self.vel - self.acceleration / 2, 0)
@@ -96,13 +138,40 @@ class PlayerCar(AbstractCar):
         self.score -= 500
 
 
-def draw(win, images, player_cars):
-    for img, pos in images:
-        win.blit(img, pos)
+def draw(win, player_cars):
+    win.fill("black")
+
+
+    pygame.draw.line(WIN, "gray", (35, 130), (35, 650))
+    pygame.draw.line(WIN, "gray", (35, 650), (160, 770))
+    pygame.draw.line(WIN, "gray", (160, 770), (670, 770))
+    pygame.draw.line(WIN, "gray", (670, 770), (760, 640))
+    pygame.draw.line(WIN, "gray", (760, 640), (760, 110))
+    pygame.draw.line(WIN, "gray", (760, 110), (670, 20))
+    pygame.draw.line(WIN, "gray", (670, 20), (430, 20))
+    pygame.draw.line(WIN, "gray", (170, 200), (170, 600))
+    pygame.draw.line(WIN, "gray", (170, 600), (240, 660))
+    pygame.draw.line(WIN, "gray", (240, 660), (580, 670))
+    pygame.draw.line(WIN, "gray", (580, 670), (660, 590))
+    pygame.draw.line(WIN, "gray", (660, 590), (660, 190))
+    pygame.draw.line(WIN, "gray", (35, 130), (130, 20))
+    pygame.draw.line(WIN, "gray", (130, 20), (220, 20))
+    pygame.draw.line(WIN, "gray", (220, 20), (300, 110))
+    pygame.draw.line(WIN, "gray", (300, 110), (300, 510))
+    pygame.draw.line(WIN, "gray", (430, 20), (370, 90))
+    pygame.draw.line(WIN, "gray", (370, 90), (370, 180))
+    pygame.draw.line(WIN, "gray", (370, 180), (420, 240))
+    pygame.draw.line(WIN, "gray", (420, 240), (500, 240))
+    pygame.draw.line(WIN, "gray", (660, 190), (620, 130))
+    pygame.draw.line(WIN, "gray", (620, 130), (500, 130))
+    pygame.draw.line(WIN, "gray", (500, 240), (560, 300))
+    pygame.draw.line(WIN, "gray", (560, 300), (560, 510))
+    pygame.draw.line(WIN, "gray", (560, 510), (500, 560))
+    pygame.draw.line(WIN, "gray", (500, 560), (330, 560))
+    pygame.draw.line(WIN, "gray", (330, 560), (300, 510))
     for car in player_cars:
         car.draw(win)
-        # car.draw_sensor(win)
-
+        # print(TRACK_BORDER_MASK.overlap_area(car.sensors))
     pygame.display.update()
 
 
@@ -126,6 +195,7 @@ def move_car(c):  # player_car
 
 
 run = True
+
 clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0)),
           (FINISH, FINISH_POSITION), (TRACK_BORDER, (0, 0))]
@@ -133,35 +203,35 @@ images = [(GRASS, (0, 0)), (TRACK, (0, 0)),
 
 # Creation of cars
 car_array = []
-for i in range(1, 3):
-    temp = PlayerCar(i, i)
+for i in range(1, 2):
+    temp = PlayerCar(i+5, i+5)
     car_array.append(temp)
-
 
 while run:
     clock.tick(FPS)
 
-    draw(WIN, images, car_array)
-
+    pygame.display.update()
+    draw(WIN, car_array)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
+
     for cars in car_array:
         move_car(cars)
-    # print(player_car.score)
+        cars.sense(WIN)
+
     for car in car_array:
-        if car.collide(TRACK_BORDER_MASK) != None:
+        if car.collide():
             car.crash()
             car.reset()
-
-        finish_poi_collide = car.collide(FINISH_MASK, *FINISH_POSITION)
+        finish_poi_collide = None
+        # finish_poi_collide = car.collide(FINISH_MASK, *FINISH_POSITION)
         if finish_poi_collide != None:
             if finish_poi_collide[1] == 0:
                 car.bounce()
             else:
                 car.score += 1000
                 print("finish")
-
 
 pygame.quit()
--- tutorial2-code/utils.py
+++ tutorial2-code/utils.py
@@ -12,13 +12,25 @@ def blit_rotate_center(win, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(
         center=image.get_rect(topleft=top_left).center)
-    center = new_rect.center
+    win.blit(rotated_image, new_rect.topleft)
+
+
+def draw_sensors(win, img, top_left, angle):
+    sensors_coordinates = []
+    center = img.get_rect(topleft=top_left).center
     while angle < 0:
         angle += 360
+    angle += 180
     radians = math.radians(angle)
-
-    for i in range(8):
+    x, y = center
+    pygame.draw.line(win, "white", center, (x + 1000 * math.sin(radians), y + 1000 * math.cos(radians)))
+    sensors_coordinates.append(center)
+    sensors_coordinates.append((x + 1000 * math.sin(radians), y + 1000 * math.cos(radians)))
+    radians -= math.pi / 4
+    for i in range(7):
         x, y = center
-        pygame.draw.line(win, "Black", center, (x + 100*math.sin(radians), y + 100*math.cos(radians)))
+        pygame.draw.line(win, "white", center, (x + 100*math.sin(radians), y + 100*math.cos(radians)))
+        sensors_coordinates.append(center)
+        sensors_coordinates.append((x + 1000 * math.sin(radians), y + 1000 * math.cos(radians)))
         radians -= math.pi/4
-    win.blit(rotated_image, new_rect.topleft)
+    return sensors_coordinates
