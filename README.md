1e125d75582af25b8a0797fe3e3feccd8550d174
dylan <ferrow.dylan.code@gmail.com>
2024-05-14 01:09:30 +0100

    fix: car collision

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Pygame-Car-Racer
+
 Make a racing game in Python using pygame!
+
+## version 5
+
+collision Major improvement
+
+### bugs
+
+the car can get stuck in the track
+the car can rotate into the track while colliding with it
+the game breaks if the computer car finishes
+the computer cars path collides with the track
---
+++ version5/utils.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+def scale_image(img, factor):
+    size = round(img.get_width() * factor), round(img.get_height() * factor)
+    return pygame.transform.scale(img, size)
+
+
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(
+        center=image.get_rect(topleft=top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
+
+
+def blit_text_center(win, font, text):
+    render = font.render(text, 1, (200, 200, 200))
+    win.blit(render, (win.get_width()/2 - render.get_width() /
+                      2, win.get_height()/2 - render.get_height()/2))
